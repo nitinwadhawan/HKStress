6ec7e5aeb8febbde3722a3288085a54e23dac1d2
Nitin <nitinwadhawan66@gmail.com>
2013-11-12 18:10:39 +0530

    working new UI gatling scala code

--- src/test/scala/basic/BasicExampleSimulation.scala
+++ src/test/scala/basic/BasicExampleSimulation.scala
@@ -11,18 +11,21 @@ import assertions._
 class BasicExampleSimulation extends Simulation {
 
         val httpProtocol = http
-                .baseURL("http://192.168.1.53:8080")
+                .baseURL("http://stagnew.healthkart.com")
                 .extraInfoExtractor((status:Status, session:Session, request:Request, response: Response) => {
-                 List[String](request.getRawUrl())
-                                                   })
+                List[String](request.getRawUrl())                                                   })
                 .acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.7")
-                .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+               .acceptHeader("application/json;q=0.9,*/*;q=0.8")
                 .acceptEncodingHeader("gzip, deflate")
                 .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
-                .disableFollowRedirect
+               .disableFollowRedirect
 
         val headers_1 = Map(
-                "Keep-Alive" -> "115")
+               """Content-Type"""-> """application/json""" ,
+               """Accept"""-> """*/*""",
+               """Accept-Encoding"""-> """gzip,deflate,sdch""",
+               """Accept-Language"""-> """en-US,en;q=0.8"""
+                )
                 
         val headers_2 = Map(
             "Cache-Control" -> """no-cache""",
@@ -34,50 +37,59 @@ class BasicExampleSimulation extends Simulation {
         val headers_3 = Map(
                 "Keep-Alive" -> "115",
                 "Content-Type" -> "application/x-www-form-urlencoded")
+                
+        val headers_4=Map(
+          "Accept"->"""application/json""",
+"Accept-Encoding" ->"""gzip,deflate,sdch""",
+"Accept-Language"  ->"""en-US,en;q=0.8""",
+"Connection" ->"""keep-alive""",
+"Content-Type" ->"""application/json; charset=UTF-8 """ ,
+"User-Agent"->"""Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36""",
+"X-Requested-With"->"""XMLHttpRequest"""
+        )        
 
         val headers_6 = Map(
                 "Accept" -> "application/json, text/javascript, */*; q=0.01",
                 "Keep-Alive" -> "115",
                 "X-Requested-With" -> "XMLHttpRequest")
+        
+                val addToCartJSON="""{"oldVariantId": "NUT2140-01","qty": "1","variantId": "37833"}"""
+                  
+                
+                
 				        val scn = scenario("Scenario name")
-                .group("Login") {
+                .repeat(1) {
                         exec(
-                                http("HK Homepage")
+                                http("HK new Homepage")
                                         .get("/")
                                         .headers(headers_1)
                                         .check(status.is(200))
                                   )                                        
-                                .pause(10)     
-                                .feed(csv("user_credentials.csv"))
-                                .exec(
+                                .pause(10)      
+                               .feed(csv("user_credentials.csv"))
+                               .exec(
                                       http("submit login")
                                      .post("/core/auth/Login.action")
-                                     .param("""email""", "${username}")
-                                     .param("""password""", "${password}")
-                                     .param("""login""", """Login""")
+                                     .param("email", "${username}")
+                                     .param("type", """old""")                                     
+				                     .param("password", "${password}")
+                                     .param("login", """Sign In""")
                                      .check(status.is(302))
                                      .check(regex("""Login using an existing account""").notExists)
                                      .headers(headers_2))                                     
                                      .pause(2,3)
-                                  .exec(
+                                .exec(
                                       http("navigate to product page")
-                                     .post("/product/muscleblaze-amino-2288/NUT3179?productReferrerId=11")
+                                     .post("/sv/muscleblaze-bcaa-2000/SP-23391?navKey=VRNT-45106")
                                      .check(status.is(200))
                                      .headers(headers_2))                                     
                                      .pause(2,3)   
                                  .exec(
                                       http("Add to Cart")
-                                     .post("/core/cart/AddToCart.action")
-                                     .param("""addToCart""", """Place Order""")
-                                     .param("""productVariantList[0]""", """HNUT235-01""")
-                                     .param("""productVariantList[0].qty""", """1""")
-                                     .param("""productVariantList[0].selected""", """true""")
-                                     /*.param("""productVariantList[1]""", """DM033-01""")
-                                     .param("""productVariantList[1].qty""", """2""")
-                                     .param("""productVariantList[1].selected""", """true""")*/
+                                     .post("/api/cart/productVariant/add")
+                                     .body(StringBody(addToCartJSON)).asJSON
                                      .check(status.is(200))
                                      .headers(headers_1))
-                                     .pause(2,3)
                                  .exec(
                                       http("Cart page")
                                      .get("/core/cart/AddToCart.action")
@@ -90,35 +102,35 @@ class BasicExampleSimulation extends Simulation {
                                      .check(status.is(200))
                                      .headers(headers_1))
                                      .pause(2,3) 
+                            /*      .exec(
+                                      http("Select Address")
+                                     .get("/core/user/SelectAddress.action")                                     
+                                     .param("selectedAddress", "${address}")
+                                     .check(status.is(200))
+                                     .headers(headers_2))                                     
+                                     .pause(2,3) */   
                                   .exec(
                                       http("Select Address")
-                                     .post("/core/user/SelectAddress.action")
-                                     .param("""selectedAddress""", "${address}")
+                                     .post("/core/user/SelectAddress.action") 
+                                     .param("selectedAddress", "${address}")
                                      .check(status.is(200))
                                      .headers(headers_2))                                     
-                                     .pause(2,3)    
-                                /*  .exec(
-                                      http("get Payment page)
-                                     .get("/core/payment/PaymentMode.action")
-                                     .check(status.is(200))
-                                     .headers(headers_2))                                     
-                                     .pause(2,3)*/
+                                     .pause(2,3)
                                   .exec(
                                       http("Order summary")
                                      .get("/core/order/OrderSummary.action")
-                                     /*.check(css(".right_container>form>input","value").saveAs("orderId"))*/
-                                     .check(css(".title>form>input","value").saveAs("orderId"))
-                                     .headers(headers_2))                                     
-                                     .pause(3,4)   
+                                     .check(css(".right_container>form>input", "value").saveAs("orderId"))
+                                     .headers(headers_2))      
+                                     .pause(2,3)   
                                   .exec(
                                       http("Confirm Payment")
                                      .post("/core/payment/CodPaymentReceive.action")
-                                     .param("""order""", """${orderId}""")
+                                     .param("""order""", "${orderId}")
                                      .param("""codContactName""", """Nitin Wadhawan""")
                                      .param("""codContactPhone""", """9910444067""")
                                      .param("""pre""", """PLACE ORDER""")
                                      .check(status.is(302))
-                                     .headers(headers_1))
+                                     .headers(headers_2))
                                      .pause(2,3)   
                                      
                         }.exec(session => {
@@ -128,7 +140,7 @@ class BasicExampleSimulation extends Simulation {
                         
 				
 
-        setUp(scn.inject(ramp(3 users) over (10 seconds)))
+        setUp(scn.inject(ramp(3 users) over (5 seconds)))
                 .protocols(httpProtocol)
 
 }
